ad3045d25e87f8908363057a007bf0810b5a6181
mariafernanda <mariafernandalibano345@gmail.com>
2024-10-30 09:20:42 -0300

    comecando

---
+++ CorridaInfinita/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CorridaInfinita;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CorridaInfinita/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CorridaInfinita;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
